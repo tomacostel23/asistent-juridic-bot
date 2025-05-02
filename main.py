67817ef7aaafb0ed9363dc631b57fcb26a22418d
tomacostel23 <toma.costel@baniifamiliei.ro>
2025-05-02 21:33:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ creds_dict = {
     "type": os.environ.get("GOOGLE_TYPE"),
     "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
     "private_key_id": os.environ.get("GOOGLE_PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+    "private_key": os.environ.get("GOOGLE_PRIVATE_KEY"),
     "client_email": os.environ.get("GOOGLE_CLIENT_EMAIL"),
     "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
     "auth_uri": os.environ.get("GOOGLE_AUTH_URI"),
